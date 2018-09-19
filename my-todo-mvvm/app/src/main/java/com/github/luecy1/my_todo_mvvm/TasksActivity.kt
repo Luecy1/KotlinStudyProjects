883378c94644e91cf2a6655f720167304d0dc0d5
luecy1 <kowaheno@gmail.com>
2018-09-19 22:04:27 +0900

    今日の分をコミット

--- my-todo-mvvm/app/src/main/java/com/github/luecy1/my_todo_mvvm/TasksActivity.kt
+++ my-todo-mvvm/app/src/main/java/com/github/luecy1/my_todo_mvvm/TasksActivity.kt
@@ -3,10 +3,10 @@ package com.github.luecy1.my_todo_mvvm
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class TasksActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.tasks_act)
     }
 }
