ae00254a4b4a83d6f744742b537c489a9335b3c8
luecy1 <kowaheno@gmail.com>
2018-09-30 22:26:04 +0900

    今日の進捗をコミット

---
+++ my-todo-mvvm/app/src/main/java/com/github/luecy1/my_todo_mvvm/TasksFilterType.kt
@@ -0,0 +1,8 @@
+package com.github.luecy1.my_todo_mvvm
+
+enum class TasksFilterType {
+
+    ALL_TASKS,
+    ACTIVE_TASKS,
+    COMPLETED_TASKS
+}
--- my-todo-mvvm/app/src/main/java/com/github/luecy1/my_todo_mvvm/TasksViewModel.kt
+++ my-todo-mvvm/app/src/main/java/com/github/luecy1/my_todo_mvvm/TasksViewModel.kt
@@ -5,7 +5,12 @@ import android.arch.lifecycle.AndroidViewModel
 import android.content.Context
 import android.databinding.ObservableArrayList
 import android.databinding.ObservableBoolean
+import android.databinding.ObservableField
 import android.databinding.ObservableList
+import android.graphics.drawable.AdaptiveIconDrawable
+import android.graphics.drawable.Drawable
+import android.support.annotation.DrawableRes
+import android.support.annotation.StringRes
 import com.github.luecy1.my_todo_mvvm.data.Task
 
 class TasksViewModel(
@@ -18,4 +23,47 @@ class TasksViewModel(
     internal val openTaskEvent = SingleLiveEvent<String>()
 
     val items:ObservableList<Task> = ObservableArrayList()
+    val dataLoading = ObservableBoolean(false)
+    val vurrentFilteringLabel = ObservableField<String>()
+    val noTaskIconRes = ObservableField<Drawable>()
+    val noTasksLabel = ObservableField<String>()
+    val empty = ObservableBoolean(false)
+    val tasksAddViewVisible = ObservableBoolean()
+    val snaclbarMessage = SingleLiveEvent<Int>()
+    val newTaskEvent = SingleLiveEvent<Void>()
+
+    var currentFiltering = TasksFilterType.ALL_TASKS
+    set(value) {
+        field = value
+        updateFiltering()
+    }
+
+    fun start(): Unit {
+        loadTasks(false)
+    }
+
+    private fun loadTasks(b: Boolean) {
+        loadTasks(b,true)
+    }
+
+    private fun loadTasks(b: Boolean, b1: Boolean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun updateFiltering() {
+        when (currentFiltering) {
+            TasksFilterType.ALL_TASKS -> {
+                //TODO
+            }
+        }
+    }
+
+    private fun setFilter(
+            @StringRes filteringLabelString:Int,
+            @StringRes noTasksLabelString :Int,
+            @DrawableRes noTaskIconDrawable: Int,
+            tasksAddVisible : Boolean
+    ) {
+        // TODO
+    }
 }
