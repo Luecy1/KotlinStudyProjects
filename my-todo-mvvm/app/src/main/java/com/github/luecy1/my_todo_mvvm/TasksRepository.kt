289f47779b37c9b0aaa3fb1a0a7845dfb4b8df75
luecy1 <kowaheno@gmail.com>
2018-10-06 14:26:49 +0900

    今日の進捗をコミット

---
+++ my-todo-mvvm/app/src/main/java/com/github/luecy1/my_todo_mvvm/TasksRepository.kt
@@ -0,0 +1,7 @@
+package com.github.luecy1.my_todo_mvvm
+
+class TasksRepository {
+    fun clearCompletedTasks() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
--- my-todo-mvvm/app/src/main/java/com/github/luecy1/my_todo_mvvm/TasksViewModel.kt
+++ my-todo-mvvm/app/src/main/java/com/github/luecy1/my_todo_mvvm/TasksViewModel.kt
@@ -7,14 +7,15 @@ import android.databinding.ObservableArrayList
 import android.databinding.ObservableBoolean
 import android.databinding.ObservableField
 import android.databinding.ObservableList
-import android.graphics.drawable.AdaptiveIconDrawable
 import android.graphics.drawable.Drawable
 import android.support.annotation.DrawableRes
 import android.support.annotation.StringRes
+import android.support.v4.content.res.ResourcesCompat
 import com.github.luecy1.my_todo_mvvm.data.Task
 
 class TasksViewModel(
-        context: Application
+        context: Application,
+        private val tasksRepository: TasksRepository
 ) : AndroidViewModel(context) {
 
     private val isDataLoadingError = ObservableBoolean(false)
@@ -22,9 +23,9 @@ class TasksViewModel(
 
     internal val openTaskEvent = SingleLiveEvent<String>()
 
-    val items:ObservableList<Task> = ObservableArrayList()
+    val items: ObservableList<Task> = ObservableArrayList()
     val dataLoading = ObservableBoolean(false)
-    val vurrentFilteringLabel = ObservableField<String>()
+    val currentFilteringLabel = ObservableField<String>()
     val noTaskIconRes = ObservableField<Drawable>()
     val noTasksLabel = ObservableField<String>()
     val empty = ObservableBoolean(false)
@@ -33,17 +34,17 @@ class TasksViewModel(
     val newTaskEvent = SingleLiveEvent<Void>()
 
     var currentFiltering = TasksFilterType.ALL_TASKS
-    set(value) {
-        field = value
-        updateFiltering()
-    }
+        set(value) {
+            field = value
+            updateFiltering()
+        }
 
     fun start(): Unit {
         loadTasks(false)
     }
 
     private fun loadTasks(b: Boolean) {
-        loadTasks(b,true)
+        loadTasks(b, true)
     }
 
     private fun loadTasks(b: Boolean, b1: Boolean) {
@@ -53,17 +54,30 @@ class TasksViewModel(
     private fun updateFiltering() {
         when (currentFiltering) {
             TasksFilterType.ALL_TASKS -> {
-                //TODO
+                setFilter(R.string.label_all,
+                        R.string.no_tasks_all,
+                        R.drawable.ic_assignment_turned_in_24dp, true)
             }
         }
     }
 
     private fun setFilter(
-            @StringRes filteringLabelString:Int,
-            @StringRes noTasksLabelString :Int,
+            @StringRes filteringLabelString: Int,
+            @StringRes noTasksLabelString: Int,
             @DrawableRes noTaskIconDrawable: Int,
-            tasksAddVisible : Boolean
+            tasksAddVisible: Boolean
     ) {
-        // TODO
+        with(context.resources) {
+            currentFilteringLabel.set(context.getString(filteringLabelString))
+            noTasksLabel.set(getString(noTasksLabelString))
+            noTaskIconRes.set(ResourcesCompat.getDrawable(this,noTaskIconDrawable,null))
+            tasksAddViewVisible.set(tasksAddVisible)
+        }
+    }
+
+    fun clearCompletedTasks() {
+        tasksRepository.clearCompletedTasks()
+        snaclbarMessage.value = R.string.completed_tasks_cleared
+        loadTasks(false,false)
     }
 }
