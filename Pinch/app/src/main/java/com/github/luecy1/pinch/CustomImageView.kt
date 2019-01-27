2e44516263e186f855b4e1483ca53a78df306b05
luecy1 <kowaheno@gmail.com>
2019-01-27 15:20:12 +0900

    Coroutineのプロジェクトを追加

--- Pinch/app/src/main/java/com/github/luecy1/pinch/CustomImageView.kt
+++ Pinch/app/src/main/java/com/github/luecy1/pinch/CustomImageView.kt
@@ -7,12 +7,9 @@ import android.view.MotionEvent
 import android.widget.ImageView
 
 class CustomImageView: ImageView {
-    constructor(context:Context) : super(context) {
-    }
-    constructor(context:Context, attrs: AttributeSet) : super(context, attrs) {
-    }
-    constructor(context:Context, attrs:AttributeSet, defStyle:Int) : super(context, attrs, defStyle) {
-    }
+    constructor(context:Context) : super(context)
+    constructor(context:Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context:Context, attrs:AttributeSet, defStyle:Int) : super(context, attrs, defStyle)
 
     override fun performClick(): Boolean {
         return true
