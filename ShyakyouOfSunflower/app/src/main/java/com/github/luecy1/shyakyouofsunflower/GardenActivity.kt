00e974f31421a6b45a1963784072f33234868ad4
luecy1 <kowaheno@gmail.com>
2019-03-23 12:14:03 +0900

    名前の変更

--- ShyakyouOfSunflower/app/src/main/java/com/github/luecy1/shyakyouofsunflower/GardenActivity.kt
+++ ShyakyouOfSunflower/app/src/main/java/com/github/luecy1/shyakyouofsunflower/GardenActivity.kt
@@ -3,10 +3,10 @@ package com.github.luecy1.shyakyouofsunflower
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class GardenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_garden)
     }
 }
