b8f60fe361a18484b64fa4252a2e68ebb469049d
luecy1 <kowaheno@gmail.com>
2019-03-24 20:22:14 +0900

    dataクラス等の作成

--- ShyakyouOfSunflower/app/src/main/java/com/github/luecy1/shyakyouofsunflower/GardenFragment.kt
+++ ShyakyouOfSunflower/app/src/main/java/com/github/luecy1/shyakyouofsunflower/GardenFragment.kt
@@ -5,15 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.luecy1.shyakyouofsunflower.adapters.GardenPlantingAdapter
+import com.github.luecy1.shyakyouofsunflower.databinding.FragmentGardenBinding
 
 class GardenFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_garden, container, false)
+        val binding = FragmentGardenBinding.inflate(inflater, container, false)
+        val adapter = GardenPlantingAdapter()
+        binding.gardenList.adapter = adapter
+        subscribeUi(adapter, binding)
+        return binding.root
     }
 
+    private fun subscribeUi(adapter: GardenPlantingAdapter, binding: FragmentGardenBinding) {
+        // TODO
+    }
 }
